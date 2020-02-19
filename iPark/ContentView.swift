d150e0ff44b4469b64254e211bc857769d699662
Krunal <kmist1@unh.newhaven.edu>
2020-02-19 15:26:59 -0500

    Initial Commit

---
+++ iPark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iPark
+//
+//  Created by Krunal Mistry on 2/19/20.
+//  Copyright © 2020 Krunal Mistry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
